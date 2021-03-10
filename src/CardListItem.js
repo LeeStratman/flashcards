f995fc9c4746cf92ddfa0578abe80414f80f3565
Lee Stratman <stratmanlc10@gmail.com>
2021-03-10 17:51:07 -0600

    Change attributes to JSX attributes.

--- src/CardListItem.js
+++ src/CardListItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const CardListItem = ({ title, subtitle }) => (
-  <div class="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+  <div className="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
     <div className="flex-1 min-w-0">
       <button className="focus:outline-none">
         <span className="absolute inset-0" aria-hidden="true"></span>
--- src/collections/NewCollectionForm.js
+++ src/collections/NewCollectionForm.js
@@ -20,7 +20,7 @@ const NewCollectionForm = ({ collections, onCreatePressed }) => {
           <div className="space-y-6 sm:space-y-5">
             <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
               <label
-                for="collection_name"
+                htmlFor="collection_name"
                 className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
               >
                 Name
