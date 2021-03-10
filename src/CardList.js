044539a54ddf4787411034fceb787c2f9653fb0a
Lee Stratman <stratmanlc10@gmail.com>
2021-03-10 14:33:08 -0600

    Add default value to CardList card prop.

--- src/CardList.js
+++ src/CardList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import CardListItem from "./CardListItem";
 
-const CardList = ({ cards }) => (
+const CardList = ({ cards = [] }) => (
   <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
     {cards.map((card) => (
       <CardListItem title={card.name} subtitle={card.name} />
