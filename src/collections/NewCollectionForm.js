92fb4a85038c7d83b034c75c780d2db3f6dff3c5
Lee Stratman <stratmanlc10@gmail.com>
2021-03-10 16:08:24 -0600

    Add form component to add new collections.

---
+++ src/collections/NewCollectionForm.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { createCollection } from "./actions";
+
+const NewCollectionForm = ({ collections, onCreatePressed }) => {
+  const [nameValue, setNameValue] = useState("");
+
+  return (
+    <>
+      <div className="space-y-8 divide-y divide-gray-200 sm:space-y-5">
+        <div className="pt-8 space-y-6 sm:pt-10 sm:space-y-5">
+          <div>
+            <h3 className="text-lg leading-6 font-medium text-gray-900">
+              New Collection
+            </h3>
+            <p className="mt-1 max-w-2xl text-sm text-gray-500">
+              Please enter a name for the collection.
+            </p>
+          </div>
+          <div className="space-y-6 sm:space-y-5">
+            <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+              <label
+                for="collection_name"
+                className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+              >
+                Name
+              </label>
+              <div className="mt-1 sm:mt-0 sm:col-span-2">
+                <input
+                  value={nameValue}
+                  onChange={(e) => setNameValue(e.target.value)}
+                  type="text"
+                  name="collection_name"
+                  id="collection_name"
+                  className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="pt-5">
+        <div className="flex justify-end">
+          <button
+            type="button"
+            className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Cancel
+          </button>
+          <button
+            onClick={() => {
+              onCreatePressed(nameValue);
+              setNameValue("");
+            }}
+            type="submit"
+            className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Save
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  collections: state.collections,
+});
+const mapDispatchToProps = (dispatch) => ({
+  onCreatePressed: (name) => dispatch(createCollection(name)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewCollectionForm);
