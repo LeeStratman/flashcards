88e457f7ebc04486896f74338674723d634591b9
Lee Stratman <stratmanlc10@gmail.com>
2021-03-10 14:10:14 -0600

    Add test data.  Add CardList component to app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import CardList from "./CardList";
+import cards from "./utils/testData";
+
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
@@ -23,7 +26,7 @@ function App() {
         <main>
           <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div className="px-4 py-8 sm:px-0">
-              <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
+              <CardList cards={cards} />
             </div>
           </div>
         </main>
