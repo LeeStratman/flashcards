dd1fe07b2ae270af77af90a551d726b39b5e8732
Lee Stratman <stratmanlc10@gmail.com>
2021-03-12 10:43:36 -0600

    Refactor H2 css.

--- src/CollectionHeader.js
+++ src/CollectionHeader.js
@@ -2,15 +2,13 @@ import React from "react";
 import FlashcardAddButton from "./flashcards/FlashcardAddButton";
 import Breadcrumbs from "./Breadcrumbs";
 
-const CollectionHeader = ({ collection, title, removeActiveCollection }) => {
+const CollectionHeader = ({ collection, title }) => {
   return (
     <>
       <Breadcrumbs />
       <div className="mt-2 md:flex md:items-center md:justify-between">
         <div className="flex-1 min-w-0">
-          <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
-            {title}
-          </h2>
+          <h2>{title}</h2>
         </div>
         <div className="mt-4 flex-shrink-0 flex md:mt-0 md:ml-4">
           <FlashcardAddButton collection={collection} />
--- src/Header.js
+++ src/Header.js
@@ -3,9 +3,7 @@ import React from "react";
 const Header = ({ title }) => (
   <div className="mt-2 md:flex md:items-center md:justify-between">
     <div className="flex-1 min-w-0">
-      <h1 className="text-3xl font-bold leading-tight text-gray-900">
-        {title}
-      </h1>
+      <h2>{title}</h2>
     </div>
   </div>
 );
