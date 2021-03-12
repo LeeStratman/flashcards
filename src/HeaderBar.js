3775d217ccc503d26ca86f29dd935832f3a7e306
Lee Stratman <stratmanlc10@gmail.com>
2021-03-12 10:04:15 -0600

    Update HeaderBar css.

--- src/HeaderBar.js
+++ src/HeaderBar.js
@@ -2,12 +2,10 @@ import React from "react";
 
 const HeaderBar = ({ title }) => (
   <div className="bg-white shadow-sm">
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div className="container">
       <div className="flex justify-between h-16">
-        <div className="flex">
-          <div className="flex-shrink-0 flex items-center">
-            <p>{title}</p>
-          </div>
+        <div className="flex-shrink-0 flex items-center">
+          <h1>{title}</h1>
         </div>
       </div>
     </div>
