fa8feb5a6033c19e19ade106e4a74ea2fc4a501d
Lee Stratman <stratmanlc10@gmail.com>
2021-03-12 14:14:19 -0600

    Fix modal close button on mobile.  Change modal width to 100% on small screens.

--- src/Modal.js
+++ src/Modal.js
@@ -14,17 +14,8 @@ const Modal = ({ open, close, children }) => {
   return (
     <div className={`fixed z-10 inset-0 overflow-y-auto ${modalClass}`}>
       <div
-        className={`flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0 ${overlayClass}`}
+        className={`flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0 ${overlayClass}`}
       >
-        {/* Background overlay, show/hide based on modal state.
-		
-					Entering: "ease-out duration-300"
-						From: "opacity-0"
-						To: "opacity-100"
-					Leaving: "ease-in duration-200"
-						From: "opacity-100"
-						To: "opacity-0" */}
-
         <div
           className={`fixed inset-0 transition-opacity ${overlayClass}`}
           aria-hidden="true"
@@ -38,23 +29,13 @@ const Modal = ({ open, close, children }) => {
         >
           &#8203;
         </span>
-
-        {/* Modal panel, show/hide based on modal state.
-		
-					Entering: "ease-out duration-300"
-						From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-						To: "opacity-100 translate-y-0 sm:scale-100"
-					Leaving: "ease-in duration-200"
-						From: "opacity-100 translate-y-0 sm:scale-100"
-						To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" */}
-
         <div
-          className={`inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6 ${panelClass}`}
+          className={`inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm w-full sm:p-6 ${panelClass}`}
           role="dialog"
           aria-modal="true"
           aria-labelledby="modal-headline"
         >
-          <div className="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
+          <div className="absolute top-0 right-0 pt-4 pr-4">
             <button
               onClick={close}
               type="button"
