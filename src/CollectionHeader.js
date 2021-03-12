b9515f1eadf914b6e4ad77fda8c9c21fb9c941ce
Lee Stratman <stratmanlc10@gmail.com>
2021-03-11 19:02:49 -0600

    Add title prop to CollectionHeader component.

--- src/CollectionHeader.js
+++ src/CollectionHeader.js
@@ -70,7 +70,7 @@ const CollectionHeader = ({ collection, title, removeActiveCollection }) => {
       <div className="mt-2 md:flex md:items-center md:justify-between">
         <div className="flex-1 min-w-0">
           <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
-            Flashcards
+            {title}
           </h2>
         </div>
         <div className="mt-4 flex-shrink-0 flex md:mt-0 md:ml-4">
