b15bd1e13fe46b7f6868045f4b2fa10e4546527a
Lee Stratman <stratmanlc10@gmail.com>
2021-03-15 14:28:56 -0500

    Refactor Eye and Pencil Icons from CollectionListItem.

--- src/collections/CollectionEditButton.js
+++ src/collections/CollectionEditButton.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Pencil } from "../icons";
 import Modal from "../Modal";
 import CollectionEditForm from "./CollectionEditForm";
 import CollectionRemoveForm from "./CollectionRemoveForm";
@@ -18,20 +19,7 @@ const CollectionEditButton = ({ collection }) => {
         }}
         className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500"
       >
-        <svg
-          className="w-5 h-5 text-gray-400"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-          />
-        </svg>
+        <Pencil />
         <span className="ml-3">Edit</span>
       </button>
       <Modal open={isOpen} close={close}>
