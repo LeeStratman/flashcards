8f44b4de62a32954b12c6ab793647b65c48d71d9
Lee Stratman <stratmanlc10@gmail.com>
2021-03-15 14:23:59 -0500

    Refact close icon from Modal component.

--- src/Modal.js
+++ src/Modal.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Close } from "./icons";
 
 const Modal = ({ open, close, children }) => {
   const overlayClass = open
@@ -42,22 +43,7 @@ const Modal = ({ open, close, children }) => {
               className="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               <span className="sr-only">Close</span>
-
-              <svg
-                className="h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
+              <Close />
             </button>
           </div>
           {open && children}
